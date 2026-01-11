bb6a5ef3e5e8306c8e060c2da33252be33383cda
sirstagg <35671212+sirstagg@users.noreply.github.com>
2026-01-11 23:43:29 +0000

    1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: './',
+    base: '/freshwater-bay-site/',
     build: {
         rollupOptions: {
             input: {
