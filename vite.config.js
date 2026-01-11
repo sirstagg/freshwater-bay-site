9287d1ac896a135ac964d2ff74e0b1f8c836a362
sirstagg <35671212+sirstagg@users.noreply.github.com>
2026-01-11 23:22:27 +0000

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // REPLACE 'freshwater-bay-site' with your exact repository name!
-  base: '/freshwater-bay-site/', 
+    base: './',
 })
